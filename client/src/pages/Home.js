bbf96148b4957e11596baec2bcba64e6db2ffa8c
javelaptop <jventura1097@gmail.com>
2019-03-09 13:57:21 -0800

    set up basic routing in client src app.js and set up basic structure for utils and pages

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+
+    render(){
+        return(
+            <h1>This is the Home Page!</h1>
+        );
+    }
+}
+
+export default Home;
---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NoMatch() {
+    return (
+        <div>
+            <h1>404 Page Not Found</h1>
+            <h1>
+                <span role="img" aria-label="Face With Rolling Eyes Emoji">
+                    🙄
+                </span>
+            </h1>
+            <a href="/">Back to Home</a>
+        </div>
+    );
+}
+
+export default NoMatch;
