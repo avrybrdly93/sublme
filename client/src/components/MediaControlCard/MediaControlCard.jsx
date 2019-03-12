183404291d36e796bc4f5a031d339d40da46dbc9
Avery <cbra067@lasierra.edu>
2019-03-11 18:12:11 -0700

    stashing changes to switch branches.  Working on like and comment

--- client/src/components/MediaControlCard/MediaControlCard.jsx
+++ client/src/components/MediaControlCard/MediaControlCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -9,6 +9,7 @@ import Typography from "@material-ui/core/Typography";
 import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
+import axios from "axios";
 
 const styles = theme => ({
   card: {
@@ -37,51 +38,133 @@ const styles = theme => ({
   }
 });
 
-function MediaControlCard(props) {
-  const { classes, theme } = props;
+class MediaControlCard extends Component {
+  constructor(props) {
+    super(props);
+    // const { classes, theme } = props;
 
-  return (
-    <Card className={classes.card}>
-      <div className={classes.details}>
-        <CardContent className={classes.content}>
-          <Typography component="h5" variant="h5" style={{ color: "black" }}>
-            Temptation
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            color="textSecondary"
-            style={{ color: "black" }}>
-            Future
-          </Typography>
-          <div className={classes.controls}>
-            <IconButton aria-label="Previous">
-              {theme.direction === "rtl" ? (
-                <SkipNextIcon />
-              ) : (
-                <SkipPreviousIcon />
-              )}
-            </IconButton>
-            <IconButton aria-label="Play/pause">
-              <PlayArrowIcon className={classes.playIcon} />
-            </IconButton>
-            <IconButton aria-label="Next">
-              {theme.direction === "rtl" ? (
-                <SkipPreviousIcon />
-              ) : (
-                <SkipNextIcon />
-              )}
-            </IconButton>
+    this.state = {
+      likes: 0,
+      comments: null
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("/api/likes")
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            likes: 2
+          });
+          console.log(result);
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    return (
+      <Card className={this.props.classes.card}>
+        <div className={this.props.classes.details}>
+          <div>
+            <button>Like - {this.state.likes}</button>
+            <button>Comment</button>
           </div>
-          <CardMedia
-            className={classes.cover}
-            image="https://thefader-res.cloudinary.com/private_images/w_640,c_limit,f_auto,q_auto:eco/future-the-wizrd-cover-680x680_uamq6x/future-new-album-documentary-the-wizrd-cover-art.jpg"
-            title="The Wizrd cover"
-          />
-        </CardContent>
-      </div>
-    </Card>
-  );
+          <CardContent className={this.props.classes.content}>
+            <Typography component="h5" variant="h5" style={{ color: "black" }}>
+              Temptation
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="textSecondary"
+              style={{ color: "black" }}
+            >
+              Future
+            </Typography>
+            <div className={this.props.classes.controls}>
+              <IconButton aria-label="Previous">
+                {/* {this.theme.direction === "rtl" ? (
+                  <SkipNextIcon />
+                ) : (
+                  <SkipPreviousIcon />
+                )} */}
+              </IconButton>
+              <IconButton aria-label="Play/pause">
+                <PlayArrowIcon className={this.props.classes.playIcon} />
+              </IconButton>
+              <IconButton aria-label="Next">
+                {/* {this.theme.direction === "rtl" ? (
+                  <SkipPreviousIcon />
+                ) : (
+                  <SkipNextIcon />
+                )} */}
+              </IconButton>
+            </div>
+            <CardMedia
+              className={this.props.classes.cover}
+              image="https://thefader-res.cloudinary.com/private_images/w_640,c_limit,f_auto,q_auto:eco/future-the-wizrd-cover-680x680_uamq6x/future-new-album-documentary-the-wizrd-cover-art.jpg"
+              title="The Wizrd cover"
+            />
+          </CardContent>
+        </div>
+      </Card>
+    );
+  }
 }
+
+// function MediaControlCard(props) {
+//   const { classes, theme } = props;
+
+//   return (
+//     <Card className={classes.card}>
+//       <div className={classes.details}>
+//         <CardContent className={classes.content}>
+//           <Typography component="h5" variant="h5" style={{ color: "black" }}>
+//             Temptation
+//           </Typography>
+//           <Typography
+//             variant="subtitle1"
+//             color="textSecondary"
+//             style={{ color: "black" }}
+//           >
+//             Future
+//           </Typography>
+//           <div className={classes.controls}>
+//             <IconButton aria-label="Previous">
+//               {theme.direction === "rtl" ? (
+//                 <SkipNextIcon />
+//               ) : (
+//                 <SkipPreviousIcon />
+//               )}
+//             </IconButton>
+//             <IconButton aria-label="Play/pause">
+//               <PlayArrowIcon className={classes.playIcon} />
+//             </IconButton>
+//             <IconButton aria-label="Next">
+//               {theme.direction === "rtl" ? (
+//                 <SkipPreviousIcon />
+//               ) : (
+//                 <SkipNextIcon />
+//               )}
+//             </IconButton>
+//           </div>
+//           <CardMedia
+//             className={classes.cover}
+//             image="https://thefader-res.cloudinary.com/private_images/w_640,c_limit,f_auto,q_auto:eco/future-the-wizrd-cover-680x680_uamq6x/future-new-album-documentary-the-wizrd-cover-art.jpg"
+//             title="The Wizrd cover"
+//           />
+//         </CardContent>
+//       </div>
+//     </Card>
+//   );
+// }
 
 MediaControlCard.propTypes = {
   classes: PropTypes.object.isRequired,
@@ -89,3 +172,5 @@ MediaControlCard.propTypes = {
 };
 
 export default withStyles(styles, { withTheme: true })(MediaControlCard);
+
+//export default MediaControlCard;
