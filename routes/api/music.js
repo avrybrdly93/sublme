cdcd232a144b293d915215fe925816f211e8d1c8
javelaptop <jventura1097@gmail.com>
2019-03-12 21:03:48 -0700

    id and genre routes

--- routes/api/music.js
+++ routes/api/music.js
@@ -3,8 +3,14 @@ const musicController=require("../../controllers/musicController");
 
 // => /api/music/
 router.route("/").get(musicController.findAll);
+
 // => /api/music/own
 router.route("/own").get(musicController.findOwnMusic);
+
 // => /api/music/genre/:genre
 router.route("/genre/:genre").get(musicController.findByGenre);
+
+// => /api/music/:id
+router.route("/:id").get(musicController.findByArtistId);
+
 module.exports=router;
