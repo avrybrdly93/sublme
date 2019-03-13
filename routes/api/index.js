5218bf6238a2b96026f6b5e3127d9249e699e4b5
javelaptop <jventura1097@gmail.com>
2019-03-12 20:25:26 -0700

    initial routes and controller file structure

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
+const musicRoutes = require("./music");
 
-// Book routes
+
+//Routes, these follow /api/ term below, (DO NOT TOUCH)
 router.use("/users", userRoutes);
+router.use("/music",musicRoutes);
+router.use("/bullhorns",bullhornRoutes);
+router.use("/messages",messageController);
 
 module.exports = router;
