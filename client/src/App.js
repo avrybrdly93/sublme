766a5d856e211a55b80f29ab218df13bf3ff5732
LidiaWebDev <lidia@fishisfast.com>
2019-03-14 21:29:15 -0700

    profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Dashboard from "./pages/dashboard";
+import Profile from "./pages/profile";
+import EditProfile from "./components/Profile/EditProfile";
 // import Songs from "./Songs.json";
+
+class DashboardPage extends Component {
+  render() {
+    return (
+      <body>
+        <Navbar />
+        <Dashboard />
+      </body>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     // console.log(Songs);
     return (
-      <React.Fragment>
-        <Navbar />
-        <Dashboard />
-      </React.Fragment>
+      <Router>
+        <React.Fragment>
+          <Route exact path="/Profile" component={Profile} />
+          <Route exact path="/Dashboard" component={DashboardPage} />
+          <Route exact path="/EditProfile" component={EditProfile} />
+        </React.Fragment>
+      </Router>
     );
   }
 }
