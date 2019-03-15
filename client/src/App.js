eb3499d5126a9684604685262c8c8a790383fb31
Campbellim <Campbellim18@gmail.com>
2019-03-14 21:23:03 -0700

    LoggedinLoggedOut

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,32 @@ import React, { Component } from "react";
 import "./App.css";
 import LoggedInNavbar from "./components/LoggedInNavbar/LoggedInNavbar";
 import Dashboard from "./pages/dashboard";
+import LoggedOutNavbar from "./components/LoggedOutNavbar/LoggedOutNavbar";
+import Cookies from "js-cookie";
 
 class App extends Component {
+  state = {
+    loggedIn: false,
+    username: "",
+    password: ""
+  };
+
+  // componentDidMount() {
+  //   if (Cookies.get("username") === undefined) {
+  //     this.setState({ loggedIn: false });
+  //   } else {
+  //     this.setState({ loggedIn: true });
+  //   }
+  // };
+
+        
   render() {
     return (
       <React.Fragment>
-        <LoggedInNavbar />
+       
+       {this.state.loggedIn ? <LoggedInNavbar /> : <LoggedOutNavbar />}
+    
+       
         <Dashboard />
       </React.Fragment>
     );
