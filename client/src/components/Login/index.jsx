5d8063c6962dae27e6ca52cabd34c2ed4dcc5355
TorresMorrissey <lookin.jazmin@outlook.com>
2019-03-19 18:04:18 -0700

    merging

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -11,7 +11,7 @@ var LoginField = React.createClass({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="stylesheet"
-          type="text/css"
+          type="css"
           media="screen"
           href="login.css"
         />
